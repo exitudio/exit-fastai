68cab0ae2927c63e673a1d11d9255fe9ec13cf1d
exitudio <exitudio@gmail.com>
2018-06-03 23:18:58 +0000

    lesson2

---
+++ planet.py
@@ -0,0 +1,11 @@
+from fastai.imports import *
+from fastai.transforms import *
+from fastai.dataset import *
+from sklearn.metrics import fbeta_score
+import warnings
+
+def f2(preds, targs, start=0.17, end=0.24, step=0.01):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return max([fbeta_score(targs, (preds>th), 2, average='samples')
+                    for th in np.arange(start,end,step)])
